2af96db746a4fca6ed1eb9861570cd8f7568ea0c
Patrick <pjm42085@creighton.edu>
2019-10-30 20:26:06 -0500

    changes

--- forms/dessertVoting/dessertVoting.js
+++ forms/dessertVoting/dessertVoting.js
@@ -3,20 +3,20 @@ var dessertList = ['Chocolate Cake','Cream Brulee','Cheesecake',
 
 dessertVoting.onshow=function(){
     drpDesserts.clear()   
-  /* add a loop here that adds all the desserts in the array 
-     to the dropdown.
-  */
+ for (i = 0; i <= dessertList.length - 1; i++)
+   drpDesserts.addItem(dessertList[i])
 }
 
 
-drpDesserts.onclick=function(){
-   // this 'if' kicks user out if they  just clicked on control 
- // but not on one item in the list.
+drpDesserts.onclick=function(s){
     if (typeof(s) == "object")   
       return                    
-    else {  // the user picked something
-        /* add code here to get item user chose and output 
-        it in a label, using a literal.
-        */
+    else { 
+      drpDesserts.value = s
+      NSB.MsgBox(`You picked ${s} - that is a great choice!`)
     }
+}
+
+btnNextPage3.onclick=function(){
+  ChangeForm(describeYou)
 }
--- forms/loginCU/loginCU.js
+++ forms/loginCU/loginCU.js
@@ -1,3 +1,14 @@
+btnLogin.onclick=function(){
+   req1 = ""
+    req1 = Ajax("https://radlab.creighton.edu/appStudio/authLDAP.php", "POST", "j_username=" + inptNetID.value + "&j_password=" + inptPass.value);
+    if (req1.status == 200) {
+        lblSuccess.value = "The authentication code is " + req1.responseText
+        ChangeForm(favFoods)
+    } else {
+        lblSuccess.value = "Error Connection Not Made: " + req1.status + " readystate " + req1.readyState + " status text " + req1.statusText;
+    }
+}
 
-
-
+btnNextPage1.onclick=function(){
+  ChangeForm(favFoods)
+}
