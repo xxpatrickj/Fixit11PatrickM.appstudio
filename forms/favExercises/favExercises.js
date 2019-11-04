97f907961a1e3430cef082649f8ad627578d6692
Patrick <pjm42085@creighton.edu>
2019-11-03 18:22:31 -0600

    final changes

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -2,11 +2,20 @@ var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
 
 favExercises.onshow=function(){
-
+    selExercises.clear()
+  for (i = 0; i <= exercises.length - 1; i++) 
+    selExercises.addItem(exercises[i])
+  for (i = 0; i <= coreExercises.length -1; i++)
+    selExercises.addItem(coreExercises[i])
 }
 
 selExercises.onfocusout=function(){
-
+let messageChoice = "You picked: " 
+    for (i = 0; i <= selExercises.text.length - 1; i++)
+        messageChoice = messageChoice + "  " + selExercises.text[i]
+        lblExercise.value = (messageChoice)
 }
 
-
+btnNextPage5.onclick=function(){
+  ChangeForm(mobileNav)
+}
